3e36b24ef2cf5f8c4090fbd14e8e314b10713090
leo <leorejwan@gmail.com>
2022-06-14 20:52:25 -0300

    Finalizando o pacote

---
+++ UrlHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UrlHelper;
+
+public static class UrlHelper
+{
+    public static string ToUrl(this string text)
+    {
+        text = text.ToLower();
+
+
+        return Regex.Replace(text, @"[^0-9a-zA-Z\._]", "");
+    }
+}
